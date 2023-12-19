68c7089500f058d24ef05397c9309a488aa59016
Victoria Arriola <arriova16@gmail.com>
2023-12-19 14:25:52 -0600

    idx works

--- darpa_cathodic_anodic.m
+++ darpa_cathodic_anodic.m
@@ -48,6 +48,9 @@ for i = 1:length(data)
     data(i).DetectionTable = detection_table{i};
     data(i).DprimeTable = dprime_table{i};
     data(i).CoeffTable = coeff_table{i};
+       pulse_string = convertCharsToStrings(data(i).Pulse); 
+   data(i).Pulse = pulse_string;
+    
     
  end
     
@@ -70,9 +73,9 @@ for d = 1:size(data,2)
             [~,b] = min(abs(yq-dprime_threshold));
             % plot out thresholds to make sure its correct
             hold on
-             plot(xq, yq, 'LineWidth', 2)
+             % plot(xq, yq, 'LineWidth', 2)
 
-            plot([0 xq(b) xq(b)], [dprime_threshold, dprime_threshold -1], 'LineStyle','--')
+            % plot([0 xq(b) xq(b)], [dprime_threshold, dprime_threshold -1], 'LineStyle','--')
 
             data(d).mechthreshold = xq(b);
         
@@ -84,48 +87,52 @@ for d = 1:size(data,2)
 end
 
 %% delta anodic - cathodic
-%trouble getting started with logic
+
 % each of the unique electrodes and if its cathodic and anodic subtract
+%doing this outside of loop because it would only give me the unique
+%electrodes of each row. not useful
 
-electrodes = vertcat(data(:).Electrodes);
-electrodes = unique(electrodes, 'rows');
+electrodes_1 = vertcat(data(:).Electrodes);
+electrodes = unique(electrodes_1, 'rows');
 
-cath_idx = strcmpi(data(:).Pulse, 'Cathodic');
-an_idx = strcmpi(data(:).Pulse, 'Anodic');
+
+pulse_data = vertcat(data(:).Pulse);
+
+cath_idx = strcmpi(pulse_data, 'Cathodic');
+an_idx = strcmpi(pulse_data, 'Anodic');
 
 
 for e = 1:size(electrodes, 1)
-   % pulse_string = convertCharsToStrings(data(i).Pulse); 
-   % data(i).Pulse = pulse_string;
-    
+
    % In data, find which rows contain those electrodes
     e_idx = find(vertcat(data(:).Electrodes) == electrodes(e, :));
 
+    
     % cath_e_idx = e_idx & cath_idx == rows which match in electrode and are cathodic
     % an_e_idx = e_idx & an_idx == rows which match in electrode and are anodic
-        deltcath(e) = data(cath_e_idx).mechthreshold - data(an_e_idx).mechthreshold;
+        % deltcath(e) = data(cath_e_idx).mechthreshold - data(an_e_idx).mechthreshold;
 
-   
-    if strcmpi(data(i).Pulse, 'Cathodic')
-
-
-    end
+   %weher
+    % if strcmpi(data(i).Pulse, 'Cathodic')
+    % 
+    % 
+    % end
 end
 
 
-   if data(i).Pulse == 'Cathodic'
-       cath_idx = find(data(i).Pulse == 'Cathodic');
-   else
-       an_idx = find(data(i).Pulse == 'Anodic');
-   end
-%wrong try again
-
-    if data(i).Electrodes == electrodes 
-        deltcath(e) = data(cath_e_idx).mechthreshold - data(an_e_idx).mechthreshold;
-    else
-        deltan = data(i).mechthreshold - data(cath_idx).mechthreshold;
-   end
-
+%    if data(i).Pulse == 'Cathodic'
+%        cath_idx = find(data(i).Pulse == 'Cathodic');
+%    else
+%        an_idx = find(data(i).Pulse == 'Anodic');
+%    end
+% %wrong try again
+% 
+%     if data(i).Electrodes == electrodes 
+%         deltcath(e) = data(cath_e_idx).mechthreshold - data(an_e_idx).mechthreshold;
+%     else
+%         deltan = data(i).mechthreshold - data(cath_idx).mechthreshold;
+%    end
+% 
 
     % if data(i).Electrodes == electrodes 
     %      deltcath = data(cath_idx).mechthreshold - data(an_idx).mechthreshold;
